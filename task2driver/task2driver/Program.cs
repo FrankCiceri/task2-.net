104e7816fd817abfa10ddb81d13ea3455e002297
FrankCiceri <frankanthonyc00@gmail.com>
2023-04-10 14:50:29 -0500

    Added newFile for palindrome and added the corresponding methods. as well as fixing some unnecessary code

--- task2driver/task2driver/Program.cs
+++ task2driver/task2driver/Program.cs
@@ -1,86 +1,15 @@
 ﻿using System.Text;
 
+
 namespace task2driver
 {
     public class TaskDriver
     {
         public static void Main(string[] args)
         {
-            palindrome();
+            Palindrome.DisplayPalindrome();
 
         }
-
-        public static void palindrome()
-        {
-            string input = Console.ReadLine();
-            int specialcharCount = 0;
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (char.IsLetter(input[i]) || input[i].Equals('-'))
-                {
-                    if(specialcharCount > 0)
-                    {
-                        sb.Append(" ");
-                        specialcharCount = 0;
-                    }
-                    sb.Append(input[i]);
-                }
-                else {
-                    specialcharCount++;
-                }
-
-            }
-
-            String[] words = sb.ToString().Split();
-            
-         
-
-            for (int i=0;i<words.Length;i++) 
-            {   
-                string word = words[i].Replace("-",string.Empty);
-                bool isPalindrome = true;
-                int front = 0;
-                int back = word.Length - 1;
-                bool different;
-                char c1;
-                char c2;
-                while (word.Length > 0 && back - front > 0 ) {
-                    c1 = Char.ToLower(word[front]);
-                    c2 = Char.ToLower(word[back]);
-                  
-                    different = (c1 != c2);
-                    if (different) { 
-                        
-                        isPalindrome = false;
-                        break;
-                    }
-                    front++;
-                    back--;                
-                
-                }
-               
-                if(word.Length > 0 ){
-                    Console.WriteLine(words[i] + " – " + (isPalindrome ? "" : "not") + " palindrome");
-                }
-
-            }
-         
-
-
-
-
-
-
-
-
-        }
-
-    }
-
-
-
-
-
+                       
+     }
 }
---
+++ task2driver/task2driver/palindrome.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2driver
+{
+    public class Palindrome
+    {
+
+        public static string BuildSB()
+        {
+            string input = Console.ReadLine();
+            int specialcharCount = 0;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]) || input[i].Equals('-'))
+                {
+                    if (specialcharCount > 0)
+                    {
+                        sb.Append(" ");
+                        specialcharCount = 0;
+                    }
+                    sb.Append(input[i]);
+                }
+                else
+                {
+                    specialcharCount++;
+                }
+
+            }
+
+            return sb.ToString();
+        }
+
+
+        public static string[] BuildList() {
+            string input = Console.ReadLine();
+            string[] words = input.Replace(",", string.Empty).Split();
+           
+            return words;
+
+        }
+
+
+
+        public static void DisplayPalindrome()
+        {
+            //string[] words = BuildSB().Split(' ');
+            string[] words = BuildList();
+            bool isLast = false;
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i].Replace("-", string.Empty);
+                bool isPalindrome = true;
+                int front = 0;
+                int back = word.Length - 1;
+                bool different;
+                char c1;
+                char c2;
+                while (word.Length > 0 && back - front > 0)
+                {
+                    c1 = Char.ToLower(word[front]);
+                    c2 = Char.ToLower(word[back]);
+
+                    different = (c1 != c2);
+                    if (different)
+                    {
+
+                        isPalindrome = false;
+                        break;
+                    }
+                    front++;
+                    back--;
+
+                }
+
+                if (word.Length > 0)
+                {
+                    if (i == words.Length - 1)
+                    {
+                        isLast = true;
+                    }
+                    Console.Write(words[i] + " – " + (isPalindrome ? "" : "not") + " palindrome" + (isLast ? ".":", "));
+                }
+
+
+
+            }
+        }
+
+    }
+}
