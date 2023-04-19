35795c33da834a7f3c872c14689c76f6f5674c3b
FrankCiceri <frankanthonyc00@gmail.com>
2023-04-19 13:00:33 -0500

    Finished classes

---
+++ OOP-Task1/OOP-Task1/Shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double CalcPerimeter();
+
+    public abstract double CalcArea();
+
+
+}
---
+++ OOP-Task1/OOP-Task1/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Triangle : Shape
+{
+    private double _a;
+    private double _b;
+    private double _c;
+    private double _height;
+    public Triangle(double a,double b, double c, double _height)
+    {
+        this._a = a; 
+        this._b = b; 
+        this._c = c;
+        this._height = _height;
+    }
+
+
+    public override double CalcArea()
+    {
+        double area = (_b * _height)/2;
+        Console.WriteLine(area);
+        return area;
+    }
+
+    public override double CalcPerimeter()
+    {
+        double perimeter = _a + _b + _c;
+        Console.WriteLine(perimeter);
+        return perimeter;
+    }
+}
